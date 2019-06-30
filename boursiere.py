1eda9d7ca74088cbe287220231bca522a2e173f8
Hugo <jacqueshugo@hotmail.fr>
2019-06-30 15:13:36 +0200

    Naming error (reserved name) correction
    round est le nom d'une fonction, on peux pas donner le nom round a une variable sans avoir des warning ou erreur.

--- boursiere.py
+++ boursiere.py
@@ -14,19 +14,23 @@ n_p = old_p + 0.1 * ((q qarder 2 - qarder 1))
 boursier = {}
 current_qarder = 0                                                      # ATTENTION : NEED TO START AT 0
 
+
 def add_beer(dico, beer_name, stock, buy_price, coef_down, coef_up, coef_max):
     dico[beer_name] = {'stock':stock, 'buy_price':buy_price, 'price':buy_price, 'coef_down':coef_down, 'coef_up':coef_up, 'coef_max':coef_max, 'q_qarder':[], 'q_current_qarder':0}
 
-def compute_new_price(buy_price,current_price,q_last_qarder , q_current_qarder ,coef_up,coef_down, coef_max, round=True):
+
+#def compute_new_price(buy_price,current_price,q_last_qarder , q_current_qarder ,coef_up,coef_down, coef_max, do_round=True):
 
     if q_current_qarder > q_last_qarder:                                # check if the actual qarder quantity sold is upper than the last
         new_price = current_price + coef_up*(q_current_qarder-q_last_qarder)
     else:
         new_price = current_price + coef_down*(q_current_qarder-q_last_qarder)
-    if new_price > (coef_max x * buy_price):                            # check for avoid too many growth in the price
+
+    if new_price > (coef_max * buy_price):                            # check for avoid too many growth in the price
         new_price = coef_max * buy_price
-    if round:
+    if do_round:
         new_price = round(new_price, 1)                                 # arround new price for 1 virgule
+
     return new_price
 
 
@@ -42,3 +46,5 @@ def update(dico, beer_name):
         current_qarder += 1
     else:
         print('--- beer not found. ---')                               # beer is not in the dico
+
+
