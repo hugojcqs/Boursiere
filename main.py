a43cc3f5a1afa333b95e46b85d11dd984e968466
Hugo <jacqueshugo@hotmail.fr>
2019-06-30 15:34:39 +0200

    Test du code #non fonctionnel

---
+++ main.py
@@ -0,0 +1,15 @@
+from BoursiereObj import Boursiere
+
+b = Boursiere()
+b.add_beer('Orval', 96, 1.5, 0.05, 0.08, 2.5)
+print(b.update_prices(do_round=False))
+b.add_conso('Orval', 5)
+print(b.update_prices(do_round=False))
+b.add_conso('Orval', 7)
+print(b.update_prices(do_round=False))
+b.add_conso('Orval', 5)
+print(b.update_prices(do_round=False))
+b.add_conso('Orval', 10)
+print(b.update_prices(do_round=False))
+b.add_conso('Orval', 2)
+print(b.update_prices(do_round=False))
