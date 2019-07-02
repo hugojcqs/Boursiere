51c902f8cd13e20c19dbe730f62b251fddf583ac
Hugo <hugo.jacques@student.unamur.be>
2019-07-02 19:36:10 +0100

    Update + un début de model SQL

---
+++ BoursiereDjango/Beer/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Beer(models.Model):
+    beer_name = models.CharField(max_length=100)
+    price = models.FloatField(null=False)
+    buy_price = models.FloatField(null=False)
+    stock = models.IntegerField(null=False)
+    coef_up = models.FloatField(null=False)
+    coef_down = models.FloatField(null=False)
+    coef_max = models.FloatField(null=False)
+    q_qarder = models.IntegerField(null=False)
+    q_current_qarder = models.IntegerField(null=False)
+    
