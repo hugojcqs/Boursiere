ac8c69fcef6489c8d7b20c67194bf6a281abfccb
lumaes <43337476+lumaes@users.noreply.github.com>
2019-07-04 23:53:46 +0200

    fix merge

--- BoursiereDjango/Beer/templatetags/filter_extra.py
+++ BoursiereDjango/Beer/templatetags/filter_extra.py
@@ -5,12 +5,15 @@ from random import *
 # make custom filter for django template
 
 register = template.Library()
+color_list = ['#',]
 
 def rcolors(value):
     #return "#"+"%06x" % random.randint(0, 0xFFFFFF)
     letters = 'B,C,D,E,F'.split(',');
     color = '#';
-    for i in range(6):
-        color += letters[math.floor(random() * len(letters))]
+    while color in color_list:
+        for i in range(6):
+            color += letters[math.floor(random() * len(letters))]
+    color_list.append(color)
     return color;
 register.filter('rcolors', rcolors)
