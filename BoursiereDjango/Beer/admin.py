022ff8a608373bf58ee5b5cbcfb703fd3e27fb42
Hugo <hugo.jacques@student.unamur.be>
2019-07-14 22:04:37 +0200

    Auto stash before revert of "Nettoyage projet"

--- BoursiereDjango/Beer/admin.py
+++ BoursiereDjango/Beer/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Beer as BeerModel
 from .models import History as HistoryModel
+from .models import Timer as TimerModel
 # Register your models here.
 admin.site.register(BeerModel)
 admin.site.register(HistoryModel)
+admin.site.register(TimerModel)
