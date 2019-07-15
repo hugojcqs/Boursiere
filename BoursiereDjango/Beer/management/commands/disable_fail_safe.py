0bde9cf9b0b7a6498b5d2693259823b2978f055b
Hugo <hugo.jacques@student.unamur.be>
2019-07-15 03:23:41 +0200

    Correction disable_fail_safe + base de données sans failsafe actif

--- BoursiereDjango/Beer/management/commands/disable_fail_safe.py
+++ BoursiereDjango/Beer/management/commands/disable_fail_safe.py
@@ -4,8 +4,11 @@ from Beer.models import Beer, History, TresoFailsafe
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        t = TresoFailsafe.objects.get(id=1)
-        t.is_activbated = False
-        t.save()
-        print(t.is_activbated)
-        print('Failsafe DISABLED - Price will now be computed using the system algorithm')
+        try:
+            t = TresoFailsafe.objects.get(id=1)
+            t.is_activated = False
+            t.save()
+            print(t.is_activated)
+            print('Failsafe DISABLED - Price will now be computed using the system algorithm')
+        except Exception as e:
+            print(e)
