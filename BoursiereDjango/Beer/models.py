2b8dd3a80436723ce6f4b5fa5a5ee506778c2f7b
Hugo <hugo.jacques@student.unamur.be>
2019-07-19 23:44:09 +0200

    correction erreur

--- BoursiereDjango/Beer/models.py
+++ BoursiereDjango/Beer/models.py
@@ -144,7 +144,7 @@ class Beer(models.Model):
             if index <= best_index:
                 best_index = index
                 beer_index_list.append((index, beer))
-            beer.ssave()
+            beer.save()
 
         beer = max(beer_index_list, key=lambda t: t[0])[1]
         print(beer)
