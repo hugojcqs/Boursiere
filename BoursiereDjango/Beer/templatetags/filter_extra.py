c5dd4174543d8385702b0ed4a959ffdf3d8bc402
lumaes <lucas.maes@student.unamur.be>
2019-07-28 15:04:41 +0200

    reduce login bar test page

--- BoursiereDjango/Beer/templatetags/filter_extra.py
+++ BoursiereDjango/Beer/templatetags/filter_extra.py
@@ -20,11 +20,6 @@ register.filter('rcolors', rcolors)
 
 def csplit(value):
     #return "#"+"%06x" % random.randint(0, 0xFFFFFF)
-    letters = 'B,C,D,E,F'.split(',');
-    color = '#';
-    while color in color_list:
-        for i in range(6):
-            color += letters[math.floor(random() * len(letters))]
-    color_list.append(color)
+
     return color;
 register.filter('split', csplit)
